1881bb0095225931d0531c24bc13e05c080da418
Cory Moore <cory@corymoore.com>
2025-08-25 00:08:57 -0500

    overriding site url

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -213,6 +213,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
         email,
         password,
         options: {
+          emailRedirectTo: `${window.location.origin}/login`,
           data: {
             first_name: firstName,
             last_name: lastName,
